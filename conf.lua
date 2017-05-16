0b12323255a51bca4b2ddc14d7c3e81ed5eb55b1
James Sral <jamessral@Jamess-MBP.attlocal.net>
2017-05-16 16:38:10 -0400

    INITIAL COMMIT

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+  t.window.title = "Swords and Sandcastles"
+  t.window.width = 800
+  t.window.height = 640
+end
---
+++ main.lua
@@ -0,0 +1,16 @@
+-- Libs Go Here
+Anim8 = require('libs/anim8')
+Bump = require('libs/bump')
+Camera = require('libs/hump/camera')
+Timer = require('libs/hump/timer')
+Vector = require('libs/hump/vector')
+
+-- Globals Go Here
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
